544add818ec0e3003cf8260b795f47f45abfa92c
Jack Burgess <jackburgess124@gmail.com>
2021-06-17 08:44:50 +0100

    apparently meta viewport needs to be here i guess?

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main id="main" />
