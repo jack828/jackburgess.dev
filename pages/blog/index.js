3b171d00bb7456e224641c0197d1625eef9d972b
Jack Burgess <jackburgess124@gmail.com>
2021-06-23 21:52:03 +0100

    static blog generation with markdown

---
+++ pages/blog/index.js
@@ -0,0 +1,46 @@
+import Container from '../../components/container'
+import MoreStories from '../../components/more-stories'
+import HeroPost from '../../components/hero-post'
+import { getAllPosts } from '../../lib/api'
+import Head from 'next/head'
+
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Head>
+        <title>Jack Burgess | Blog</title>
+      </Head>
+      <Container>
+        Intro
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.coverImage}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt'
+  ])
+
+  return {
+    props: { allPosts }
+  }
+}
