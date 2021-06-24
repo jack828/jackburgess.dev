d2b619d3e223bb6008e719a49123f4876918886c
Jack Burgess <jackburgess124@gmail.com>
2021-06-24 20:38:12 +0100

    preview warning in header

--- components/blog-header.js
+++ components/blog-header.js
@@ -1,9 +1,16 @@
 import { DateFormatter, CoverImage, BlogTitle } from '../components'
 
-const BlogHeader = ({ title, coverImage, date }) => {
+const BlogHeader = ({ title, coverImage, date, published }) => {
   return (
     <>
       <BlogTitle>{title}</BlogTitle>
+      {!published && (
+        <div className="message is-danger">
+          <div className="message-header">
+            <p className="mx-auto">This blog has not been published yet.</p>
+          </div>
+        </div>
+      )}
       <div className="mb-8">
         <CoverImage title={title} src={coverImage} height={620} width={1240} />
       </div>
