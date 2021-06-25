ebc4e27fa5316a5d5ebde6645dc8b12c1509f39e
Jack Burgess <jackburgess124@gmail.com>
2021-06-25 14:35:36 +0100

    blog title responsiveness

--- components/blog-title.js
+++ components/blog-title.js
@@ -1,6 +1,6 @@
 const BlogTitle = ({ children }) => {
   return (
-    <h1 className="title is-size-1 pt-4">
+    <h1 className="title is-size-1-desktop is-size-2-tablet is-size-3-mobile pt-4">
       {children}
     </h1>
   )
