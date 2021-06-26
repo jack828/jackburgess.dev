394816c97be02b00b8bc4f64ef6a15736ae62f7b
Jack Burgess <jackburgess124@gmail.com>
2021-06-26 23:21:41 +0100

    invert container padding

--- components/container.js
+++ components/container.js
@@ -1,11 +1,11 @@
 import classnames from 'classnames'
 
-const Container = ({ fluid, noPadding, children }) => {
+const Container = ({ fluid, padded, children }) => {
   return (
     <div
       className={classnames('container', {
         'is-fluid': fluid,
-        'p-5': !noPadding
+        'p-5': padded
       })}
     >
       {children}
