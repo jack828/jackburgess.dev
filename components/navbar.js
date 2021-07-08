5c65f8adeee0cd7e4e6e62428add0630d8680a57
Jack Burgess <jackburgess124@gmail.com>
2021-07-08 23:13:33 +0100

    remove hamburger nav cos it sucks with react-headroom

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import classnames from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -6,13 +5,11 @@ import Headroom from 'react-headroom'
 
 const Navbar = () => {
   const router = useRouter()
-  const [open, setOpen] = useState(false)
-  const toggleOpen = () => setOpen((prevState) => !prevState)
   const isOnHome = router.asPath === '/'
   const isOnBlog = router.asPath.startsWith('/blog')
 
   return (
-    <Headroom>
+    <Headroom wrapperStyle={{ width: '100%' }}>
       <nav
         className="navbar is-primary"
         role="navigation"
@@ -22,25 +19,9 @@ const Navbar = () => {
           <Link href="/" passHref>
             <a className="navbar-item">jackburgess.dev</a>
           </Link>
-
-          <a
-            role="button"
-            className={classnames('navbar-burger', { 'is-active': open })}
-            onClick={toggleOpen}
-            aria-label="menu"
-            aria-expanded={open ? 'true' : 'false'}
-            data-target="navbar"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
         </div>
 
-        <div
-          id="navbar"
-          className={classnames('navbar-menu', { 'is-active': open })}
-        >
+        <div id="navbar" className="navbar-end">
           <div className="navbar-start">
             <Link href="/" passHref>
               <a
