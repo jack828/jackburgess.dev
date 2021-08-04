b8e0ecb3f7aea2355a92191818125c2549c4730f
Jack Burgess <jackburgess124@gmail.com>
2021-08-04 22:48:15 +0100

    feat(blog): better content sizing & spacing in page

--- components/blog-body/index.js
+++ components/blog-body/index.js
@@ -1,9 +1,13 @@
 const BlogBody = ({ content }) => {
   return (
-    <div
-      className="content content-override"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <div className="columns is-centered">
+      <div className="column is-four-fifths">
+        <div
+          className="content content-override"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    </div>
   )
 }
 
