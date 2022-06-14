61b61e7401933aeeceddff0812ff44e5e0eb8374
Jack Burgess <jackburgess124@gmail.com>
2022-06-14 22:34:32 +0100

    fix: only sri if not server

--- next.config.js
+++ next.config.js
@@ -75,18 +75,17 @@ module.exports = withBundleAnalyzer({
       use: ['@svgr/webpack']
     })
 
-    if (!dev) {
+    if (!dev && !isServer) {
       config.output.crossOriginLoading = 'anonymous'
       config.plugins.push(new SubresourceIntegrityPlugin())
-      if (!isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat'
-        })
-      }
+
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
     }
 
     return config
