fb35d139ab1affe6cb9244e30e54adf5fcdce6dd
Jack Burgess <jackburgess124@gmail.com>
2022-07-17 12:51:47 +0100

    chore: remove _cf-beacon function

--- functions/_cf-beacon.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestGet() {
-  const response = await fetch(
-    'https://static.cloudflareinsights.com/beacon.min.js'
-  )
-
-  return new Response(response.body, response)
-}
