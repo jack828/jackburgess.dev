2078f915c36881919901bbc0bdb99561cadeef4e
Jack Burgess <jackburgess124@gmail.com>
2022-07-17 18:53:27 +0100

    fix: meta theme-color correction for light/dark modes

--- pages/_document.js
+++ pages/_document.js
@@ -57,7 +57,16 @@ class MyDocument extends Document {
           />
           {/* Chrome */}
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <meta name="theme-color" content="#000000" />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#fff"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#000"
+          />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
